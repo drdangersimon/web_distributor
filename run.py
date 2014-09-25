69aff932cb6d4600ff7102759bf123cceedf823d
Thuso Simon <dr.danger.simon@gmail.com>
2014-09-25 12:56:13 +0200

    got base class to work

--- run.py
+++ run.py
@@ -27,26 +27,27 @@ class Server(object):
         self.context = zmq.Context()
         self.cur_workers = {}
         self.initalize()
+        # other varibles
+        self.done = False
         
     def initalize(self):
         '''starts all sockets'''
         # send
-        self.gal_send = self.context.socket(zmq.DEALER)
+        self.gal_send = self.context.socket(zmq.PUSH)
         self.gal_send.bind("tcp://127.0.0.1:%i"%self.sendport)
         self.gal_send.setsockopt(zmq.IDENTITY, b'gal send')
         # recive results
-        self.results_receiver = self.context.socket(zmq.DEALER)
+        self.results_receiver = self.context.socket(zmq.PULL)
         self.results_receiver.bind("tcp://127.0.0.1:%i"%self.recivport)
         self.results_receiver.setsockopt(zmq.IDENTITY, b'gal recive')
         # manage
-        self.control_sender = self.context.socket(zmq.DEALER)
+        self.control_sender = self.context.socket(zmq.PUB)
         self.control_sender.bind("tcp://127.0.0.1:%i"%self.manageport)
         self.control_sender.setsockopt(zmq.IDENTITY, b'gal manage')
         # make poll
         self.poller = zmq.Poller()
-        self.poller.register(self.gal_send, zmq.POLLIN|zmq.POLLOUT)
         self.poller.register(self.results_receiver, zmq.POLLIN)
-        self.poller.register(self.control_sender, zmq.POLLIN|zmq.POLLOUT)
+        #
         
 
         # load in galaxies
@@ -57,34 +58,46 @@ class Server(object):
             time.sleep(1)
             # check results
             socks = dict(self.poller.poll(1000))           
-            if self.gal_send in socks:
-                # send galaxy data
-                if socks[self.gal_send] == zmq.POLLIN:
-                    # recive
-                    print 'recive gal'
-                    print self.gal_send.recv()
-                
-                else:
-                    #send
-                    self.gal_send.send_pyobj(([1],[1,2]))
-                    print 'send gal'
-                
             if self.results_receiver in socks:
-                print 'results'
-            if self.control_sender in socks:
-                print 'control'
+                # get results
+                msg = None
+                msg, id, py_obj = self.results_receiver.recv_pyobj(zmq.NOBLOCK)
+                if msg == 'need data' and not self.done:
+                    # Send data and record worker
+                    print 'sending data to %s'%id
+                    data = self.get_next()
+                    self.update(id, 0)
+                    self.gal_send.send_pyobj((id, data))
+                elif msg == 'status':
+                    # log status
+                    print 'update from %s'%id
+                    self.update(id, py_obj)
+                elif msg == 'done':
+                    # get results and send more data or tell processes to finish
+                     print '%s finished'%id
+                     self.finalize(id, py_obj)
+            # check if finished
+            if self.done:
+                self.close()
+            
  
     def save(self):
         pass
                
-    def send(self, data, param):
-        '''send data down a zeromq "PUSH" connection to be processed by 
-        listening workers, in a round robin load balanced fashion.'''
-        pass
-    def update(self):
-        pass
+    def get_next(self):
+        '''Gets next data to send and checks how many gal are left'''
+        print 'this many are left'
+        self.done = True
+        return None, None
+    def update(self, id, ess, chains=None):
+        '''Keeps track of all data that is being processed and prints message
+        about status'''
+    def finalize(self, id, chains):
+        '''When data is finished. Saves and removes from working list'''
+        pass    
     def close(self):
-        pass
+        '''Send mesgage for all processers to close'''
+        self.control_sender.send('exit')
 
 class Client(object):
     '''
@@ -103,48 +116,73 @@ class Client(object):
         self.host_addr = host_addr
         self.context = zmq.Context()
         # initalize contex managers
-        self.work_receiver = self.context.socket(zmq.REQ)
+        # gets data to work on
+        self.work_receiver = self.context.socket(zmq.PULL)
         self.work_receiver.connect("tcp://%s:%i"%(self.host_addr, self.recivport))
         self.work_receiver.setsockopt(zmq.IDENTITY, self.id )
-        self.results_sender = self.context.socket(zmq.REQ)
+        # send results and status
+        self.results_sender = self.context.socket(zmq.PUSH)
         self.results_sender.connect("tcp://%s:%i"%(self.host_addr, self.sendport))
         self.results_sender.setsockopt(zmq.IDENTITY, self.id )
-        self.control_receiver = self.context.socket(zmq.REQ)
+        # gets info when to quit
+        self.control_receiver = self.context.socket(zmq.SUB)
         self.control_receiver.connect("tcp://%s:%i"%(self.host_addr,self.manageport))
-        self.control_receiver.setsockopt(zmq.IDENTITY, self.id )
+        self.control_receiver.setsockopt(zmq.SUBSCRIBE, '')
         # make poller
         self.poller = zmq.Poller()
-        self.poller.register(self.work_receiver, zmq.POLLIN|zmq.POLLOUT)
-        self.poller.register(self.results_sender, zmq.POLLOUT)
-        self.poller.register(self.control_receiver, zmq.POLLIN|zmq.POLLOUT)
+        self.poller.register(self.work_receiver, zmq.POLLIN)
+        self.poller.register(self.control_receiver, zmq.POLLIN)
         
     def get_data(self):
         '''Reqests data for processing'''
         # tell client that it's ready
-        self.work_receiver.send(b'ready')
-        data, param = self.work_receiver.recv_pyobj()
+        self.results_sender.send_pyobj((b'need data', self.id, []))
+        # check is all work is done
+        done = self.check_done()
+        if done:
+            return None, None
+        while True:
+            try:
+                id, pyobj= self.work_receiver.recv_pyobj(zmq.NOBLOCK)
+                if id == self.id:
+                    data, param = pyobj
+                    break
+            except zmq.Again:
+                print 'trouble reciving data try again in 5 seconds'
+                time.sleep(5)
+            
         print 'recived_data'
-        # check if exit message
-        if data is None:
-            sys.exit(0)
         return data, param
     
     def send_update(self, ess):
         '''Sends effective sample size to client'''
         # send client report
-        self.control_receiver.send_pyobj(ess)
+        self.results_sender.send_pyobj(('status',self.id, ess))
 
     def send_results(self, results):
         '''When done sends results'''
         # send results
-        self.results_sender.send_pyobj(results)
+        self.results_sender.send_pyobj(('done', self.id, results))
+        # check if need to quit
 
+    def check_done(self):
+        '''Checks if I should finish'''    
+        socket = self.poller.poll(1000)
+        if self.control_receiver in socket:
+            msg = self.control_receiver.recv(zmq.NOBLOCK)
+            if msg == 'exit':
+                print 'Finishing'
+                return True
+        return False
+    
 def test_worker():
     '''Stars worker and does tests'''
     print 'starting worker'
-    client = Client('localhost')
+    client = Client('127.0.0.1')
     data, param = client.get_data()
-    print data
+    client.send_update(999)
+    client.send_results((data,param,[]))
+    print 'Done'
 
 def test_server():
     print 'starting server'
